eb3084babfd8deebb3f4b605d3a17c1e1b610219
Yoreni <yoreni@icloud.com>
2022-02-28 10:33:19 +0000

    tried to update to 1.18.1
    i almost did it but i couldnt figure out of to remove the vanilia status effect bars when you open your inventory. in the minecraft class they seem to be private methods but i dont know how to inject in to these cos it wont let me.

--- src/main/java/a/yoreni/potioneffectmod/mixin/InventoryScreenMixin.java
+++ src/main/java/a/yoreni/potioneffectmod/mixin/InventoryScreenMixin.java
@@ -10,12 +10,14 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@SuppressWarnings("rawtypes")
 @Mixin(AbstractInventoryScreen.class)
-public class InventoryScreenMixin<T extends ScreenHandler> extends HandledScreen<T>
+public abstract class InventoryScreenMixin <T extends ScreenHandler> extends HandledScreen<T>
 {
     public InventoryScreenMixin(T handler, PlayerInventory inventory, Text title)
     {
@@ -31,29 +33,33 @@ public class InventoryScreenMixin<T extends ScreenHandler> extends HandledScreen
 
     //code below coppied and pasted from https://github.com/HellsingDarge/CompactStatusEffects/blob/master/src/main/java/me/hellsingdarge/compactstatuseffects/mixins/AbstractInventoryScreenMixin.java
     //thx
-    @Inject(method = "applyStatusEffectOffset", at = @At(value = "TAIL"))
+   /* @Inject(method = "applyStatusEffectOffset", at = @At(value = "TAIL"))
     public void applyStatusEffectOffset(CallbackInfo ci)
     {
         this.x = (this.width - this.backgroundWidth) / 2;
-    }
+    }*/
 
-    @Inject(method = "drawStatusEffects", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/AbstractInventoryScreen;drawStatusEffectBackgrounds(Lnet/minecraft/client/util/math/MatrixStack;IILjava/lang/Iterable;)V"), cancellable = true)
+    /*
+    @Mutable
+    @Inject(method = "drawStatusEffects", at = @At(value = "HEAD"), cancellable = true)
     public void drawBackground(MatrixStack matrixStack, CallbackInfo ci)
     {
         ci.cancel();
-    }
+    }*/
+
 
     @Inject(method = "drawStatusEffectSprites", at = @At("HEAD"), cancellable = true)
-    void drawSprite(MatrixStack matrixStack, int i, int j, Iterable<StatusEffectInstance> iterable, CallbackInfo ci)
+    private void drawSprite(MatrixStack matrices, int x, int height, Iterable<StatusEffectInstance> statusEffects, CallbackInfo ci)
     {
         ci.cancel();
     }
 
+    /*
     @Inject(method = "drawStatusEffectDescriptions", at = @At("HEAD"), cancellable = true)
     void drawDescriptions(MatrixStack matrixStack, int i, int j, Iterable<StatusEffectInstance> iterable, CallbackInfo ci)
     {
         ci.cancel();
-    }
+    }*/
 
     @Override
     protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY)
